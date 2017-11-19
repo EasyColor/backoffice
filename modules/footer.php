1bb9f48ef6e106b4092939b770ba4c2c9c34bec5
bernardotavares <bernardoptavares@gmail.com>
2017-11-19 16:12:17 +0000

    php esqueleto

---
+++ modules/footer.php
@@ -0,0 +1,25 @@
+</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<script src="lib/bootstrap-colorselector-0.2.0/js/bootstrap-colorselector.js"></script>
+<script src="lib/google-code-prettify/prettify.js"></script>
+
+<script>
+    $(function() {
+
+        $('#colorselector_1').colorselector();
+        $('#colorselector_2').colorselector();
+        $('#colorselector_3').colorselector();
+
+    });
+</script>
+
+</body>
+
+</html>
+
